3a40f973621ebfe94219887fe6add5761e23f947
Phillip Reyland <phillip@bytedept.com>
2017-06-09 13:04:56 -0400

    updates

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+	consumer_key: 			'BCMSRX4IPE5Jm6qyP74tupWAv',
+	consumer_secret: 		'BFAfwsdHSFazrpLmNOYEGzZiMi5QgVdBamNyzBCz8UzuovCsvZ',
+	access_token: 			'861704396052324353-VQQGdliLlZk0WnS7wJpPId64VpdhTwA',
+	access_token_secret: 	'a1PAvjO1fDDQQ5IxvZXqyyGk1k7fE6mAOj0JdPy9j1bHk'
+}
